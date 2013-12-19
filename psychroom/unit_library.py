92a13b29115f0dff89fdb21a2f23de3a21e3e04e
Andrew Hjortland <hjortlanda@gmail.com>
2013-12-18 19:59:12 -0500

    added some more unit types to the unit_library

--- psychroom/unit_library.py
+++ psychroom/unit_library.py
@@ -84,4 +84,161 @@ def unit_library():
         ),
     }
 
+    # Units of Length
+    length = {
+        'meter': props(
+            quantity='length',
+            name='meter',
+            symbol='m',
+            base='meter',
+            to_base=BASE_TO_BASE,
+            from_base=BASE_TO_BASE
+        ),
+        'inch': props(
+            quantity='length',
+            name='inch',
+            symbol='in',
+            base='meter',
+            to_base=lambda x: 0.0254 * x,
+            from_base=lambda x: x / 0.0254
+        ),
+        'foot': props(
+            quantity='length',
+            name='inch',
+            symbol='ft',
+            base='meter',
+            to_base=lambda x: 0.3048 * x,
+            from_base=lambda x: x / 0.3048
+        ),
+        'yard': props(
+            quantity='length',
+            name='inch',
+            symbol='yd',
+            base='meter',
+            to_base=lambda x: 0.9144 * x,
+            from_base=lambda x: x / 0.9144
+        ),
+        'mile': props(
+            quantity='length',
+            name='inch',
+            symbol='mi',
+            base='meter',
+            to_base=lambda x: 1609.344 * x,
+            from_base=lambda x: x / 1609.344
+        ),
+    }
+
+    # Units of Mass
+    mass = {
+        'gram': props(
+            quantity='mass',
+            name='gram',
+            symbol='g',
+            base='gram',
+            to_base=BASE_TO_BASE,
+            from_base=BASE_TO_BASE
+        ),
+        'tonne': props(
+            quantity='mass',
+            name='tonne',
+            symbol='t',
+            base='g',
+            to_base=lambda x: 1000E3 * x,
+            from_base=lambda x: x / 1000E3
+        ),
+        'slug': props(
+            quantity='mass',
+            name='slug',
+            symbol='sl',
+            base='g',
+            to_base=lambda x: 14594 * x,
+            from_base=lambda x: x / 14594
+        ),
+        'pound (mass)': props(
+            quantity='mass',
+            name='pound (mass)',
+            symbol='lbm',
+            base='g',
+            to_base=lambda x: 453.6 * x,
+            from_base=lambda x: x / 453.6
+        ),
+    }
+
+    # Units of time
+    time = {
+        'second': props(
+            quantity='time',
+            name='second',
+            symbol='s',
+            base='s',
+            to_base=BASE_TO_BASE,
+            from_base=BASE_TO_BASE
+        ),
+        'minute': props(
+            quantity='time',
+            name='minute',
+            symbol='min',
+            base='s',
+            to_base=lambda x: 60. * x,
+            from_base=lambda x: x / 60.
+        ),
+        'hour': props(
+            quantity='time',
+            name='hour',
+            symbol='hr',
+            base='s',
+            to_base=lambda x: 3600. * x,
+            from_base=lambda x: x / 3600.
+        ),
+        'day': props(
+            quantity='time',
+            name='day',
+            symbol='d',
+            base='s',
+            to_base=lambda x: 86400. * x,
+            from_base=lambda x: x / 86400.
+        ),
+        'year': props(
+            quantity='time',
+            name='year',
+            symbol='yr',
+            base='s',
+            to_base=lambda x: 365. * 24. * 60. * 60. * x,
+            from_base=lambda x: x / (365. * 24. * 60. * 60.)
+        ),
+    }
+
+    # Units of electric current
+    current = {
+        'ampere': props(
+            quantity='electric current',
+            name='ampere',
+            symbol='A',
+            base='A',
+            to_base=BASE_TO_BASE,
+            from_base=BASE_TO_BASE
+        ),
+    }
+
+    # Units of electric potential
+    voltage = {
+        'voltage': props(
+            quantity='electric potential',
+            name='voltage',
+            symbol='V',
+            base='V',
+            to_base=BASE_TO_BASE,
+            from_base=BASE_TO_BASE
+        ),
+    }
+
+    library = dict(
+        list(library.items()) +
+        list(length.items()) +
+        list(mass.items()) +
+        list(time.items()) +
+        list(current.items()) +
+        list(voltage.items())
+    )
+
     return library
